e89b3c77ad28c5d9383fe6ab0fa87060aef3cd09
andrei.orasteanu <andrei.orasteanu8@gmail.com>
2018-07-13 15:08:55 +0300

    day1

---
+++ 3 FrontEnd/Code/Day 1/frontdev/scripts/app.js
@@ -0,0 +1,134 @@
+var employeesList = [
+    {
+        firstName: 'John',
+        lastName: 'King',
+        phone: '0123456789',
+        salary: 4500
+    },
+    {
+        firstName: 'Steven',
+        lastName: 'Gerrard',
+        phone: '0123456789',
+        salary: 4500
+    },
+    {
+        firstName: 'Diana',
+        lastName: 'Ross',
+        phone: '0123456789',
+        salary: 4500
+    },
+    {
+        firstName: 'Mike',
+        lastName: 'Bob',
+        phone: '0123456789',
+        salary: 4500
+    },
+    {
+        firstName: 'Emily',
+        lastName: 'Hudson',
+        phone: '0123456789',
+        salary: 4500
+    }
+
+];
+var x = false;
+
+function showList() {
+    var avg = averageSalary();
+    var myTable = '<table id="employeeTable" class="table table-striped"><tr><th>First Name</th><th>Last Name</th><th>Phone</th><th>Salary</th><th><button onclick="convert()">Convert salary</button></th><th> </th><th></th></tr>';
+    for (var i in employeesList) {
+        myTable += '<tr><td>' + employeesList[i].firstName + '</td><td>' + employeesList[i].lastName + '</td><td>' + employeesList[i].phone + '</td> <td>' + employeesList[i].salary + '</td><td class="eurosalary"></td><td><button onclick="vizualizare('+i+')">Vizualizare</button></td><td><button onclick="stergere('+i+')">Stergere</button></td></tr>';
+    }
+    myTable +='<tr><td>' + '1' + '</td><td>' + '2' + '</td><td>' + '3' + '</td><td>' + parseInt(avg) + '</td></tr>';
+    myTable += '</table>'
+    var container = document.getElementById('listcontainer');
+    container.innerHTML = myTable;
+}
+
+function convert() {
+    x = true;
+    var i = 0;
+    var y = document.getElementsByClassName("eurosalary");
+    for (var j in y) {
+        y[j].innerHTML = euroSalary(x, employeesList[i].salary);
+        i++;
+    }
+
+}
+
+var Employee = function (firstName, lastName, phone, salary) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phone = phone;
+    this.salary = salary;
+}
+
+function addEmployee() {
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var phone = document.getElementById("phone").value;
+    var salary = document.getElementById("salary").value;
+    employeesList.push(new Employee(firstName, lastName, phone, salary));
+    showList();
+    salaryTotal();
+}
+
+function salaryTotal() {
+    var salaryTotal = 0;
+    for (var i in employeesList) {
+        salaryTotal += parseInt(employeesList[i].salary.toString());
+
+    }
+    var salariuTotal = document.getElementById("salaryTotal");
+    salariuTotal.innerText = salaryTotal;
+}
+
+function deleteLastEmployee() {
+    employeesList.pop();
+    showList();
+    salaryTotal();
+
+}
+
+function euroSalary(x, salary) {
+
+    if (x == true)
+        return salary / 4.5;
+    if (x == false)
+        return ' ';
+}
+
+function vizualizare(i) {
+    alert('First Name: ' + employeesList[i].firstName + '    Last Name: ' + employeesList[i].lastName + '    Phone: ' + employeesList[i].phone + '    Salary: ' + employeesList[i].salary);
+}
+
+function stergere(i){
+    employeesList.splice(i, 1);
+    showList();
+}
+
+function averageSalary(){
+    var tot = 0;
+    for(var i in employeesList){
+        tot += parseInt(employeesList[i].salary.toString());
+    }
+    var average = parseFloat(tot.toString()) / employeesList.length;
+    return average;
+}
+
+function sortBy() {
+    var num = document.getElementById("in");
+    if(num == 1){
+
+    }
+    if(num == 2){
+
+    }
+    if(num == 3){
+
+    }
+    if(num == 4){
+
+    }
+
+}
