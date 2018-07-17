c9bdf82b2fbd7361256fd0ecfce47f34039a8204
andrei.orasteanu <andrei.orasteanu8@gmail.com>
2018-07-17 14:55:18 +0300

    day2

--- 3 FrontEnd/Code/Day 3/scripts/app.js
+++ 3 FrontEnd/Code/Day 3/scripts/app.js
@@ -1,10 +1,29 @@
-// TODO #2 load ngRoute module
-
+var hrApp = angular.module('hrApp', ['ngRoute']);
 
 // TODO #3 add default route for main page
-// TODO #4 add #/numbers route and use redirectTo
+
+hrApp.config(['$routeProvider'], function($routeProvider){
+    $routeProvider.when('/', {
+        templeteUrl: 'views/main.html',
+        controller: 'MainController'
+    })
+    $routeProvider.when('/numbers', {
+        redirectTo: 'math'
+    })
+    $routeProvider.when('/math', {
+        templateUrl: 'views/demo/math.html',
+        controller: 'MathController'
+    })
+
+})
+
+
 // TODO #6 add route for mathematical operations
-// TODO #9 add route for http request demo page
+    .when('/demoRequest', {
+        templateUrl: 'views/demo/request.html',
+        controller: 'RequestController'
+    })
+
 
 // TODO #HR1 add routes for Employees List page and Employee View page
 
--- 3 FrontEnd/Code/Day 3/scripts/services/CommonResourcesFactory.js
+++ 3 FrontEnd/Code/Day 3/scripts/services/CommonResourcesFactory.js
@@ -1,30 +1,31 @@
-hrApp.factory('commonResourcesFactory', function () {
-        var baseUrl = "http://10.16.8.77:8181/hrapp/";
+'use strict';
+
+hrApp.factory('CommonResourcesFactory', function() {
+        var baseUrl = "/app/mvc/";
         return {
-           findAllDepartmentsUrl : baseUrl + "departments/findAll",
-           findAllEmployeesUrl : baseUrl + "employees/findAll",
-           findAllJobsUrl : baseUrl + "jobs/findAll",
-           findAllLocationsUrl : baseUrl + "locations/findAll",
-           findOneDepartmentUrl : baseUrl + "departments/findOne/",
-           findOneEmployeeUrl : baseUrl + "employees/findOne/",
-           findOneJobUrl : baseUrl + "jobs/findOne/",
-           findOneLocationUrl : baseUrl + "locations/findOne/",
-           deleteDepartmentUrl : baseUrl + "departments/delete",
-           deleteEmployeeUrl : baseUrl + "employees/delete",
-           deleteJobUrl : baseUrl + "jobs/delete",
-           deleteLocationUrl : baseUrl + "locations/delete",
-           addDepartmentUrl : baseUrl + "departments/create",
-           addEmployeeUrl : baseUrl + "employees/create",
-           addJobUrl : baseUrl + "jobs/create",
-           addLocationUrl : baseUrl + "locations/create",
-           editDepartmentUrl : baseUrl + "departments/edit",
-           editEmployeeUrl : baseUrl + "employees/edit",
-           editJobUrl : baseUrl + "jobs/edit",
-           editLocationUrl : baseUrl + "locations/edit"
+            findAllDepartmentsUrl: baseUrl + "departments/all",
+            findAllEmployeesUrl: baseUrl + "employees/all",
+            findAllJobsUrl: baseUrl + "jobs/all",
+            findAllLocationsUrl: baseUrl + "locations/all",
+            findOneDepartmentUrl: baseUrl + "departments/one",
+            findOneEmployeeUrl: baseUrl + "employees/one",
+            findOneJobUrl: baseUrl + "jobs/one",
+            findOneLocationUrl: baseUrl + "locations/one",
+            deleteDepartmentUrl: baseUrl + "departments/one",
+            deleteEmployeeUrl: baseUrl + "employees/one",
+            deleteJobUrl: baseUrl + "jobs/one",
+            deleteLocationUrl: baseUrl + "locations/one",
+            addDepartmentUrl: baseUrl + "departments/create",
+            addEmployeeUrl: baseUrl + "employees/create",
+            addJobUrl: baseUrl + "jobs/create",
+            addLocationUrl: baseUrl + "locations/create",
+            editDepartmentUrl: baseUrl + "departments/edit",
+            editEmployeeUrl: baseUrl + "employees/edit",
+            editJobUrl: baseUrl + "jobs/edit",
+            editLocationUrl: baseUrl + "locations/edit"
         };
     }
 );
-
 hrApp.factory('commonResourcesFactoryBackup', function () {
         var baseUrl = "http://hrapp-zth.rhcloud.com/hrapp/";
         return {
