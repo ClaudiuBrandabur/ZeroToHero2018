3a302596e0ec8acc57d4afc82244cc28961f5b7b
diana527maria <diana.cataros@gmail.com>
2018-07-20 12:45:42 +0300

    AngularJS Day 4

---
+++ 3 FrontEnd/Code/Day 4/project4/scripts/controllers/ColorsController.js
@@ -0,0 +1,38 @@
+colorApp.controller('ColorsController', ['$scope', function ($scope) {
+    $scope.colors = [
+        {
+            text: "muted",
+            class: "text-muted",
+            type: "strong"
+        },
+        {
+            text: "primary",
+            class: "text-primary",
+            type: "strong"
+        },
+        {
+            text: "success",
+            class: "text-success",
+            type: "strong"
+        },
+        {
+            text: "info",
+            class: "text-info",
+            type: "boring"
+        },
+        {
+            text: "warning",
+            class: "text-warning",
+            type: "boring"
+        },
+        {
+            text: "danger",
+            class: "text-danger",
+            type: "boring"
+        }
+    ];
+    $scope.selectedClass='';
+    $scope.changeColor = function() {
+        $scope.selected = $scope.selectedClass.class;
+    };
+}]);
