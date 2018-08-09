a4adb4bdbd66b9e34a25fe166d1a2576aa1d3c40
diana527maria <diana.cataros@gmail.com>
2018-08-09 12:40:06 +0300

    Maven

---
+++ 4 Java/4_5 Maven+UnitTesting/HelloWorldExtend/src/test/java/ro/teamnet/hello2/HelloWorldExtendTest.java
@@ -0,0 +1,10 @@
+package ro.teamnet.hello2;
+
+import org.junit.Test;
+
+public class HelloWorldExtendTest {
+    @Test
+    public void test() throws Exception {
+        new HelloWorldExtend().extendSayHello();
+    }
+}
