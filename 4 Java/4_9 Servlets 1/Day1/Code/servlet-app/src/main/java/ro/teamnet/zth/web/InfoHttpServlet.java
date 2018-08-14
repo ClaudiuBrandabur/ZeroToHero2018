56c9be903602d7476db31dc5e1e412506970c6ae
andreea.gorgoi <andreeagorgoi@gmail.com>
2018-08-14 10:30:31 +0300

    servlets

--- 4 Java/4_9 Servlets 1/Day1/Code/servlet-app/src/main/java/ro/teamnet/zth/web/InfoHttpServlet.java
+++ 4 Java/4_9 Servlets 1/Day1/Code/servlet-app/src/main/java/ro/teamnet/zth/web/InfoHttpServlet.java
@@ -15,15 +15,45 @@ public class InfoHttpServlet extends HttpServlet {
 
         resp.setContentType("text/html");
         Enumeration<String> headers = req.getHeaderNames();
+        Enumeration<String> values = null;
 
         String table = "<table>";
+        table += "<tr>" + "<td> headers </td>" +  "<td> values </td>" + "<td> cookie </td>" + "<td> method </td>" + "<td> query </td> </tr>";
 
-        while(headers.hasMoreElements()){
-            String myCurrentheader = headers.nextElement();
+
+        while(headers.hasMoreElements()) {
+            String myCurrentHeader = headers.nextElement();
             table += "<tr>";
-            table += "<td>" + myCurrentheader + "</td>";
-            table += "</tr>";
+            table += "<td>" + myCurrentHeader + "</td>";
+
+            values = req.getHeaders(myCurrentHeader);
+            while (values.hasMoreElements()) {
+                String myCurrentValue = values.nextElement();
+                table += "<th>" + myCurrentValue + "</th>";
+            }
+
+            table += "<th>" + req.getCookies()+ "</th>";
         }
+
+        table += "<th>" + req.getMethod() + "</th>";
+        table += "<th>" + req.getQueryString() + "</th>";
+        table += "</tr>";
+        table +="</table>";
+
+        table += "<table>" + "<tr>" + "<td> param </td>" +  "<td> values </td> </tr>" ;
+
+        Enumeration <String> params = req.getParameterNames();
+
+        while(params.hasMoreElements()) {
+            String myCurrentParam = params.nextElement();
+            table += "<tr>";
+            table += "<td>" + myCurrentParam + "</td>";
+            String value = req.getParameter(myCurrentParam);
+            table += "<th>" + value + "</th>";
+        }
+
+        table += " </table> ";
+
 
         PrintWriter pw = resp.getWriter();
         pw.write(table);
