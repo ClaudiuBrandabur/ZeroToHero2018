ba9a7664edc0385b37b72e1d09a767067d6b4b8b
andreea.gorgoi <andreeagorgoi@gmail.com>
2018-08-17 17:49:49 +0300

    mvc2

--- 4 Java/4_12 MVC 2/MVC 2/src/main/java/ro/teamnet/zth/Z2HDispatcherServlet.java
+++ 4 Java/4_12 MVC 2/MVC 2/src/main/java/ro/teamnet/zth/Z2HDispatcherServlet.java
@@ -1,17 +1,30 @@
 package ro.teamnet.zth;
 
+import org.codehaus.jackson.map.ObjectMapper;
+import ro.teamnet.zth.fmk.MethodAttributes;
 import ro.teamnet.zth.fmk.domain.HttpMethod;
+import ro.teamnet.zth.utils.BeanDeserializator;
+import ro.teamnet.zth.utils.ControllerScanner;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Z2HDispatcherServlet extends HttpServlet {
 
+    Map<String,MethodAttributes> allMyAttributes = new HashMap<>();
+    ControllerScanner cscanner;
+
     @Override
     public void init() throws ServletException {
+        cscanner = new ControllerScanner("ro.teamnet.zth.appl.controllers");
+        cscanner.scan();
     }
 
     @Override
@@ -52,12 +65,41 @@ public class Z2HDispatcherServlet extends HttpServlet {
     }
 
     private void reply(HttpServletResponse resp, Object resultToDisplay) {
+        ObjectMapper om = new ObjectMapper();
+        final String responseAsString;
+        try {
+            responseAsString = om.writeValueAsString(resultToDisplay);
+            resp.getWriter().write(responseAsString);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
     private Object dispatch(HttpServletRequest req, HttpMethod methodType) {
+
+        String URL = req.getPathInfo();
+        MethodAttributes methodAttribute;
+
+        if(!URL.startsWith("/employees") && !URL.startsWith("/departments") && !URL.startsWith("/locations") && !URL.startsWith("/jobs")){
+            throw new RuntimeException("URL GRESIT!");
+        }
+
+        methodAttribute = cscanner.getMetaData(URL,methodType);
+
+        try {
+
+            if(methodAttribute != null) {
+
+                Class myClass = methodAttribute.getControllerClass();
+                Method myMethod = methodAttribute.getMethod();
+                return myMethod.invoke(myClass.newInstance(),BeanDeserializator.getMethodParams(myMethod,req).toArray());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
-
-
 }
