3ed646aaeff078770c3e4f0588f217653f5944a0
andrei.orasteanu <andrei.orasteanu8@gmail.com>
2018-08-21 15:39:39 +0300

    JDBC

---
+++ 4 Java/4_8 JDBC/Code/JDBCApp/src/main/java/ro/teamnet/zth/api/database/DBManager.java
@@ -0,0 +1,47 @@
+package ro.teamnet.zth.api.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBManager {
+    private DBManager() throws UnsupportedOperationException{
+    }
+    private final static String CONNECTION_STRING = "jdbc:oracle:thin:@" + DBProperties.IP + ":" + DBProperties.PORT;
+    static void registerDriver() throws ClassNotFoundException{
+        try {
+            Class.forName(DBProperties.DRIVER_CLASS);
+        }
+        catch(ClassNotFoundException ex) {
+            System.out.println("Error: unable to load driver class!");
+            System.exit(1);
+        }
+
+    }
+
+    public static Connection getConnection() throws ClassNotFoundException, SQLException{
+        Connection connection = null;
+        registerDriver();
+        try {
+            connection = DriverManager.getConnection(CONNECTION_STRING, DBProperties.USER, DBProperties.PASS);
+        }
+        catch(SQLException se){
+            se.printStackTrace();
+        }
+        return connection;
+    }
+    public static boolean checkConnection(Connection connection) throws SQLException {
+        Statement statement = null;
+        statement = connection.createStatement();
+        try {
+            statement.executeQuery("SELECT 1 FROM DUAL");
+            return true;
+        }
+         catch (SQLException se){
+            se.printStackTrace();
+            return false;
+        }
+    }
+}
+
---
+++ 4 Java/4_8 JDBC/Code/JDBCApp/src/main/java/ro/teamnet/zth/api/database/DBProperties.java
@@ -0,0 +1,10 @@
+package ro.teamnet.zth.api.database;
+
+public interface DBProperties {
+    String IP = "localhost";
+    String PORT = "1521";
+    String USER = "system"; // user from SQL workshop
+    String PASS = "andrei123"; // pass from SQL workshop
+    String DRIVER_CLASS = "oracle.jdbc.driver.OracleDriver";
+
+}
