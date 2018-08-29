bfc36a4afb5009333d4745ea305cc447cd4f580b
sorin.bottea <sorin.bottea@teamnet.ro>
2018-08-29 14:54:16 +0300

    Day 1 ex suplimentare

--- 3 FrontEnd/Code/Day 1/frontdev/scripts/app.js
+++ 3 FrontEnd/Code/Day 1/frontdev/scripts/app.js
@@ -3,7 +3,7 @@ var employeesList = [
         firstName: 'John',
         lastName: 'King',
         phone: '0123456789',
-        salary: 4500
+        salary: 4500,
     },
     {
         firstName: 'Steven',
@@ -32,24 +32,39 @@ var employeesList = [
 ];
 
 function showList() {
-    var myTable = '<table class="table table-striped"><tr><th>First Name</th><th>Last Name</th><th>Phone</th><th>Salary</th></tr>';
+    var myTable = '<table class="table table-striped">' +
+        '<tr>' +
+        '<th>First Name</th>' +
+        '<th>Last Name</th>' +
+        '<th>Phone</th>' +
+        '<th>Salary</th>' +
+        '<th><button onclick="convertSalary()">Convert Salary</button></th>' +
+        '<th>Vizualizare</th>' +
+        '<th>Stergere</th>'+
+        '</tr>';
     for (var i in employeesList) {
         myTable +=
 
-            '<tr><td>' + employeesList[i].firstName + '</td><td>' + employeesList[i].lastName + '</td><td>' + employeesList[i].phone + '</td><td>' + employeesList[i].salary + '</td></tr>';
+            '<tr>' +
+            '<td>' + employeesList[i].firstName + '</td>' +
+            '<td>' + employeesList[i].lastName + '</td>' +
+            '<td>' + employeesList[i].phone + '</td>' +
+            '<td>' + employeesList[i].salary + '</td>' +
+            '<td><th><button onclick="visualise('+i+')">Vizualizare</button></td>' +
+            '<td><button onclick="stergere('+i+')">Stergere</button></td>' +
+            '</tr>';
     }
     myTable += ' </table>';
     var container = document.getElementById('listcontainer');
     container.innerHTML = myTable;
 }
 
-var Employee = function (firstName, lastName, phone, salary) {
+var Employee = function (firstName, lastName, phone, salary, eurovalue) {
 
     this.firstName = firstName;
     this.lastName = lastName;
     this.phone = phone;
     this.salary = salary;
-
 };
 
 function addEmployee() {
@@ -61,7 +76,10 @@ function addEmployee() {
     employeesList.push(new Employee(firstName, lastName, phone, salary));
     showList();
 }
-
+function input(){
+    var nr = document.getElementById("nr").value;
+    nr.push(new nr);
+}
 function salaryTotal() {
     var total = 0;
     for (var i in employeesList) {
@@ -73,4 +91,41 @@ function salaryTotal() {
 function deleteLast() {
     employeesList.pop();
     showList();
+}
+function convertSalary() {
+    var myTable = '<table class="table table-striped">' +
+        '<tr>' +
+        '<th>First Name</th>' +
+        '<th>Last Name</th>' +
+        '<th>Phone</th>' +
+        '<th>Salary</th>' +
+        '<th><button onclick="convertSalary()">Convert Salary</button></th>' +
+        '<th>Vizualizare</th>' +
+        '<th>Stergere</th>' +
+        '</tr>';
+    for (var i in employeesList) {
+        myTable +=
+
+            '<tr>' +
+            '<td>' + employeesList[i].firstName + '</td>' +
+            '<td>' + employeesList[i].lastName + '</td>' +
+            '<td>' + employeesList[i].phone + '</td>' +
+            '<td>' + employeesList[i].salary + '</td>' +
+            '<td>' + parseFloat(employeesList[i].salary/4.5).toFixed(2) + '</td>' +
+            '<td><button onclick="visualise('+i+')">Vizualizare</button></td>' +
+            '<td><button onclick="stergere('+i+')">Stergere</button></td>' +
+            '</tr>';
+    }
+    myTable += ' </table>';
+    var container = document.getElementById('listcontainer');
+    container.innerHTML = myTable;
+}
+function visualise(i) {
+       alert( employeesList[i].firstName+" "+employeesList[i].lastName+" "+employeesList[i].phone+" "+employeesList[i].salary);
+
+}
+function stergere(i) {
+    employeesList.splice(i,1);
+    showList()
+
 }
