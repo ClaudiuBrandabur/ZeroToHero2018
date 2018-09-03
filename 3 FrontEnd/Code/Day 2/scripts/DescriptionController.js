e236d8221a646f17427737deb7731b7d9a2c73eb
sorin.bottea <sorin.bottea@teamnet.ro>
2018-09-03 18:24:27 +0300

    day2 app.js

--- 3 FrontEnd/Code/Day 2/scripts/DescriptionController.js
+++ 3 FrontEnd/Code/Day 2/scripts/DescriptionController.js
@@ -1,5 +1,14 @@
- $scope.descriptionShow = true;
+angular.module('hrApp').controller('ScopesController', ['$rootScope','$scope', function ($rootScope, $scope) {
+    $scope.title = 'Two Way Binding Demo';
+    $scope.childtemplate = 'templates/childscope.html';
+    $scope.resetFirstVariable = function () {
+        $scope.firstVariable = undefined;
+    };
+    $scope.setFirstVariable = function (val) {
+        $scope.firstVariable = val;
+    };
 
+    $scope.descriptionShow = true;
     $scope.toggleDescriptionShow = function () {
 
         if ($scope.descriptionShow === true) {
