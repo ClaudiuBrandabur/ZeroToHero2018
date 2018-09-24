254d2b161fa64d7f4bf54db8b6121f1912e33241
sorin.bottea <sorin.bottea@teamnet.ro>
2018-09-24 12:16:01 +0300

    MAven 1

---
+++ 4 Java/4_5 Maven+UnitTesting/dev/asd/src/main/java/ro/teamnet/hello/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+
+    public void sayHello(){
+        System.out.println("Hello World!");
+    }
+
+    public String returnHelloKey(){
+        return "HelloKey";
+    }
+
+    public static void main(String [] args){
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.sayHello();
+    }
+}
